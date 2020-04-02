825a561eab717f7957af61da492a61b50ee974b9
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-02 10:53:02 -0300

    adicionados modelos de objetos

---
+++ src/src/model/Franquia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.model;
+
+/**
+ *
+ * @author arthur
+ */
+public class Franquia {
+    public int id;
+    public String endereco;
+    public String cnpj;
+    public boolean ativo;
+    public int cidade_id;
+}
---
+++ src/src/model/Funcionario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.model;
+
+/**
+ *
+ * @author arthur
+ */
+public class Funcionario {
+    public String cpf;
+    public String nome;
+    public String nome_usuario;
+    public String senha;
+    public String endereco;
+    public boolean ativo;
+    public int cidade_id;
+    public int franquia_id;
+    public int contato_id;
+}
