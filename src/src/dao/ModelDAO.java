9c46754a3be74a272987b9bc4b3ce1d352866c6e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-09 13:10:41 -0300

    adicionada interface para todos os DAO

---
+++ src/src/dao/ModelDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.dao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author arthur
+ */
+public interface ModelDAO <T> {
+    
+    public void save(T objeto);
+    
+    public void update(T objeto);
+    
+    public void delete(T objeto);
+    
+    public ArrayList<T> getAll();
+    
+    public ArrayList<T> search(String criterio);
+    
+    public T get(String id);
+}
