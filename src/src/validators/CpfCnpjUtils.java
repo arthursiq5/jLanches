ddc2459edca3ed8f66d173a4800e6b6a4b9375c3
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-22 01:25:18 -0300

    adequado aos padrões do projeto

--- src/src/validators/CpfCnpjUtils.java
+++ src/src/validators/CpfCnpjUtils.java
@@ -13,8 +13,11 @@ public class CpfCnpjUtils {
     private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
     private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
-    public static boolean isValid(String cpfCnpj) {
-        return (isValidCPF(cpfCnpj) || isValidCNPJ(cpfCnpj));
+    public static ValidationAnswers isValid(String cpfCnpj) {
+        if(isValidCPF(cpfCnpj) == ValidationAnswers.PASS ||
+           isValidCNPJ(cpfCnpj) == ValidationAnswers.PASS)
+                return ValidationAnswers.PASS;
+        return ValidationAnswers.FAIL;
     }
 
     private static int calcularDigito(String str, int[] peso) {
@@ -31,26 +34,30 @@ public class CpfCnpjUtils {
         return String.format("%11s", text).replace(' ', character);
     }
 
-    private static boolean isValidCPF(String cpf) {
+    private static ValidationAnswers isValidCPF(String cpf) {
         cpf = cpf.trim().replace(".", "").replace("-", "");
-        if ((cpf==null) || (cpf.length()!=11)) return false;
+        if ((cpf==null) || (cpf.length()!=11)) return ValidationAnswers.FAIL;
 
         for (int j = 0; j < 10; j++)
             if (padLeft(Integer.toString(j), Character.forDigit(j, 10)).equals(cpf))
-                return false;
+                return ValidationAnswers.FAIL;
 
         Integer digito1 = calcularDigito(cpf.substring(0,9), pesoCPF);
         Integer digito2 = calcularDigito(cpf.substring(0,9) + digito1, pesoCPF);
-        return cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString());
+        if(cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString()))
+            return ValidationAnswers.PASS;
+        return ValidationAnswers.FAIL;
     }
 
-    private static boolean isValidCNPJ(String cnpj) {
+    private static ValidationAnswers isValidCNPJ(String cnpj) {
         cnpj = cnpj.trim().replace(".", "").replace("-", "");
-        if ((cnpj==null)||(cnpj.length()!=14)) return false;
+        if ((cnpj==null)||(cnpj.length()!=14)) return ValidationAnswers.FAIL;
 
         Integer digito1 = calcularDigito(cnpj.substring(0,12), pesoCNPJ);
         Integer digito2 = calcularDigito(cnpj.substring(0,12) + digito1, pesoCNPJ);
-        return cnpj.equals(cnpj.substring(0,12) + digito1.toString() + digito2.toString());
+        if(cnpj.equals(cnpj.substring(0,12) + digito1.toString() + digito2.toString()))
+            return ValidationAnswers.PASS;
+        return ValidationAnswers.FAIL;
     }
 }
 
