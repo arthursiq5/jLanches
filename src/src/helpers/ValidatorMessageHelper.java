466784f187fc055bb2b4c5d24df3194173e48977
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-22 01:13:17 -0300

    criado helper para mensagens de validação

---
+++ src/src/helpers/ValidatorMessageHelper.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.helpers;
+
+/**
+ *
+ * @author arthur
+ */
+public class ValidatorMessageHelper {
+    public static boolean alert(String message){
+        MessageHelper.createInfoMessage("Aviso", message);
+        return false;
+    }
+    
+    public static boolean alertCombo(String message){
+        return ValidatorMessageHelper.alert("O campo "+message + " precisa ser selecionado");
+    }
+    
+    public static boolean alertEndereco(){
+        return ValidatorMessageHelper.alert("Por favor, selecione um endereço");
+    }
+    
+    public static boolean alertID(){
+        return ValidatorMessageHelper.alert("Para atualizar, é necessário um ID válido");
+    }
+}
