58fd6edcf0573d183245310129380f0befab4655
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-22 00:30:38 -0300

    adicionados validators para estados

---
+++ src/src/validators/BasicValidators.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.validators;
+
+/**
+ *
+ * @author arthur
+ */
+public abstract class BasicValidators {
+    public static ValidationAnswers isEmpty(String message){
+        if("".equals(message))
+            return ValidationAnswers.FAIL;
+        return ValidationAnswers.PASS;
+    }
+    
+    public static ValidationAnswers isTooLong(String message, int maxLength){
+        if(message.length() >= maxLength)
+            return ValidationAnswers.FAIL;
+        return ValidationAnswers.PASS;
+    }
+    
+    public static ValidationAnswers hasIncorrectLength(String message, int maxLength){
+        if(message.length() == maxLength)
+            return ValidationAnswers.PASS;
+        return ValidationAnswers.FAIL;
+    }
+    
+    public static ValidationAnswers isZeroOrNegative(int number){
+        if(number <= 0)
+            return ValidationAnswers.FAIL;
+        return ValidationAnswers.PASS;
+    }
+}
