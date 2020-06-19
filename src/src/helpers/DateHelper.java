655baa635104ef2b454f104a4b18de77a6ff6a50
Arthur Siqueira <arthur.siq5@gmail.com>
2020-06-19 07:08:42 -0300

    retornando vetores para os dias e anos possíveis

--- src/src/helpers/DateHelper.java
+++ src/src/helpers/DateHelper.java
@@ -7,9 +7,11 @@ package src.helpers;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import src.constants.DateFormats;
+import src.constants.Meses;
 
 /**
  *
@@ -27,6 +29,33 @@ public class DateHelper {
         return formato.parse(date);
     }
     
+    public static int[] monthToDays(Meses mes){
+        int[] dias = new int[mes.getQuantidadeDeDias()];
+        
+        for (int i = 0; i < mes.getQuantidadeDeDias(); i++)
+            dias[i] = i+1;
+        
+        return dias;
+    }
+    
+    public static int getCurrentYear(){
+        Calendar anoAtual = Calendar.getInstance();
+        return anoAtual.get(Calendar.YEAR);
+    }
+    
+    public static int[] getPossibleYears(){
+        int anoMinimo = 1970;
+        int anoMaximo = DateHelper.getCurrentYear() + 11;
+        
+        int[] anosPossiveis = new int[anoMaximo-anoMinimo];
+        
+        for (int i = 0; i < (anoMaximo-anoMinimo); i++) {
+            anosPossiveis[i] = anoMinimo+i;
+        }
+        
+        return anosPossiveis;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Dia de hoje: " + DateHelper.dateToString(new Date()));
         
@@ -34,6 +63,10 @@ public class DateHelper {
             System.out.println("Data de hoje: " + DateHelper.stringToDate("2020-05-15 13:00:25"));
         } catch (Exception e) {
             System.out.println(e);
+        }
+        
+        for (int possibleYear : DateHelper.getPossibleYears()) {
+            System.out.println("Ano: "+ possibleYear);
         }
     }
 }
