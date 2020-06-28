c6802f5c233fbd16034eaad7ca447fd932eeea6f
Arthur Siqueira <arthur.siq5@gmail.com>
2020-06-28 15:19:19 -0300

    transferido elemento para pasta à parte

--- src/com/jlanches/src/views/internal/frame/advanced/add/item/AddItemPedido.java
+++ src/com/jlanches/src/views/internal/frame/advanced/add/item/AddItemPedido.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jlanches.src.views;
+package com.jlanches.src.views.internal.frame.advanced.add.item;
 
 import java.util.ArrayList;
 import com.jlanches.src.helpers.FrameHelper;
